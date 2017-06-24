4efdbab066c0e42f2de40562a7cc2d5fd434c7b9
eduardo.voltolini <eduardoftl@hotmail.com>
2017-06-23 22:06:36 -0300

    Commit correto

--- MyEyesPlus/MyEyes_Interface/MainActivity.cs
+++ MyEyesPlus/MyEyes_Interface/MainActivity.cs
@@ -3,7 +3,6 @@ using Android.Widget;
 using Android.OS;
 using Android.Content;
 using Android.Graphics;
-using Xamarin.Android;
 using Android.Provider;
 using Android.Runtime;
 
@@ -17,22 +16,14 @@ namespace MyEyes
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            /*public void IniciarCamera() {
-                Intent intent = new Intent(MediaStore.ActionImageCapture);
-                StartActivityForResult(intent, 0);
-            }
-
-            protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
-            {
-                base.OnActivityResult(requestCode, resultCode, data);
-                global::Android.Graphics.Bitmap bitmap = (Bitmap)data.Extras.Get("data");
-                imgView1.SetImageBitmap(bitmap);
-            }*/
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
 
             // Criação de botões
             Button BtnCapturar = FindViewById<Button>(Resource.Id.Capturar);
             Button BtnRepetir = FindViewById<Button>(Resource.Id.Repetir);
             Button BtnVoltar = FindViewById<Button>(Resource.Id.Voltar);
+            ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
 
             // Definição dos metodos
             BtnCapturar.Click += delegate { Capturar(); };
@@ -40,20 +31,19 @@ namespace MyEyes
             BtnVoltar.Click += delegate { Voltar(); };
         }
 
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap imageBitmap = (Bitmap)data.Extras.Get("data");
+            ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
+            CmpImageCamera.SetImageBitmap(imageBitmap);
+        }
+
+
         public void Capturar()
         {
-            //define o alerta para executar a tarefa
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            AlertDialog alerta = builder.Create();
-            //Define o Titulo
-            alerta.SetTitle("Simulador sem câmera");
-            alerta.SetIcon(Android.Resource.Drawable.IcDialogAlert);
-            alerta.SetMessage("Por estar utilizando um emulador, não tem câmera.");
-            alerta.SetButton("OK", (s, ev) =>
-            {
-                Toast.MakeText(this, "Legal, vamos continuar... !", ToastLength.Short).Show();
-            });
-            alerta.Show();
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
         }
 
         public void Repetir()
@@ -84,4 +74,3 @@ namespace MyEyes
         }
     }
 }
-
