6e5ca831c66733f7b03831abb1dab36c41d2e694
eduardo.voltolini <eduardoftl@hotmail.com>
2017-06-24 01:35:30 -0300

    commit correto 2

--- MyEyesPlus/MyEyes_Interface/MainActivity.cs
+++ MyEyesPlus/MyEyes_Interface/MainActivity.cs
@@ -1,6 +1,10 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Graphics;
+using Android.Provider;
+using Android.Runtime;
 
 namespace MyEyes
 {
@@ -12,12 +16,14 @@ namespace MyEyes
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            IniciarCamera();
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
 
             // Criação de botões
             Button BtnCapturar = FindViewById<Button>(Resource.Id.Capturar);
             Button BtnRepetir = FindViewById<Button>(Resource.Id.Repetir);
             Button BtnVoltar = FindViewById<Button>(Resource.Id.Voltar);
+            ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
 
             // Definição dos metodos
             BtnCapturar.Click += delegate { Capturar(); };
@@ -25,25 +31,19 @@ namespace MyEyes
             BtnVoltar.Click += delegate { Voltar(); };
         }
 
-        public void IniciarCamera() {
-            // Intent intent = new Intent(MediaStore.ActionImageCapture);
-            // StartActivityForResult(intent, 0);
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap imageBitmap = (Bitmap)data.Extras.Get("data");
+            ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
+            CmpImageCamera.SetImageBitmap(imageBitmap);
         }
+
 
         public void Capturar()
         {
-            //define o alerta para executar a tarefa
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            AlertDialog alerta = builder.Create();
-            //Define o Titulo
-            alerta.SetTitle("Simulador sem câmera");
-            alerta.SetIcon(Android.Resource.Drawable.IcDialogAlert);
-            alerta.SetMessage("Por estar utilizando um emulador, não tem câmera.");
-            alerta.SetButton("OK", (s, ev) =>
-            {
-                Toast.MakeText(this, "Legal, vamos continuar... !", ToastLength.Short).Show();
-            });
-            alerta.Show();
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
         }
 
         public void Repetir()
@@ -51,37 +51,21 @@ namespace MyEyes
 
         }
 
-
-        protected void onResume()
-        {
-           /* if (getIntent().getBooleanExtra("SAIR", false))
-            {
-                finish();
-            }
-            super.onResume();*/
-        }
-
         public void Voltar()
         {
-            //define o alerta para executar a tarefa
             AlertDialog.Builder alerta = new AlertDialog.Builder(this);
             alerta.SetTitle("Sair");
             alerta.SetIcon(Android.Resource.Drawable.IcDialogAlert);
             alerta.SetMessage("Deseja realmente sair");
-            //define o botão positivo
             alerta.SetPositiveButton("Ok", (senderAlert, args) =>
             {
-                //this.finish();
+                Finish();
             });
-            //define o botão negativo
             alerta.SetNegativeButton("Cancelar", (senderAlert, args) =>
-            {
-                Toast.MakeText(this, "Cancelado !", ToastLength.Short).Show();
-            });
-            //cria o alerta e exibe
+            {});
+
             Dialog dialog = alerta.Create();
             dialog.Show();
         }
     }
 }
-
