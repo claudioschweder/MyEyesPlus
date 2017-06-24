aa6a813bbca0e8ad61fbf0c4a4e1dffd2b349e24
eduardo.voltolini <eduardoftl@hotmail.com>
2017-06-24 02:56:43 -0300

    AD

--- MyEyesPlus/MyEyes_Interface/MainActivity.cs
+++ MyEyesPlus/MyEyes_Interface/MainActivity.cs
@@ -5,42 +5,63 @@ using Android.Content;
 using Android.Graphics;
 using Android.Provider;
 using Android.Runtime;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.IO;
 
 namespace MyEyes
 {
     [Activity(Label = "MyEyes", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private int byteArray;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            Intent intent = new Intent(MediaStore.ActionImageCapture);
-            StartActivityForResult(intent, 0);
-
             // Criação de botões
-            Button BtnCapturar = FindViewById<Button>(Resource.Id.Capturar);
+            Button BtnCamera = FindViewById<Button>(Resource.Id.Camera);
             Button BtnRepetir = FindViewById<Button>(Resource.Id.Repetir);
-            Button BtnVoltar = FindViewById<Button>(Resource.Id.Voltar);
+            Button BtnFechar = FindViewById<Button>(Resource.Id.Fechar);
             ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
 
             // Definição dos metodos
-            BtnCapturar.Click += delegate { Capturar(); };
+            BtnCamera.Click += delegate { Camera(); };
             BtnRepetir.Click += delegate { Repetir(); };
-            BtnVoltar.Click += delegate { Voltar(); };
+            BtnFechar.Click += delegate { Fechar(); };
         }
 
         protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
             Bitmap imageBitmap = (Bitmap)data.Extras.Get("data");
+
             ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
             CmpImageCamera.SetImageBitmap(imageBitmap);
         }
 
+        public static async Task<string> GetStreamStringFromImageSourceAsync(Xamarin.Forms.StreamImageSource imageSource, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (imageSource.Stream != null)
+            {
+                var stream = await imageSource.Stream(cancellationToken);
 
-        public void Capturar()
+                //stream?.Length > 0 can do this - Pubudu
+                // stream
+                if (stream == null || stream.Length <= 0) return null;
+
+                var bytes = new byte[stream.Length];
+                stream.Position = 0;
+                stream.Read(bytes, 0, (int)stream.Length);
+                return Convert.ToBase64String(bytes);
+            }
+            return null;
+        }
+
+        public void Camera()
         {
             Intent intent = new Intent(MediaStore.ActionImageCapture);
             StartActivityForResult(intent, 0);
@@ -51,12 +72,12 @@ namespace MyEyes
 
         }
 
-        public void Voltar()
+        public void Fechar()
         {
             AlertDialog.Builder alerta = new AlertDialog.Builder(this);
             alerta.SetTitle("Sair");
             alerta.SetIcon(Android.Resource.Drawable.IcDialogAlert);
-            alerta.SetMessage("Deseja realmente sair");
+            alerta.SetMessage("Deseja realmente fechar o aplicativo?");
             alerta.SetPositiveButton("Ok", (senderAlert, args) =>
             {
                 Finish();
