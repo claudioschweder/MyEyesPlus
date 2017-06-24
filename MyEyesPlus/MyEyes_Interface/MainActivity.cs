ca54f4fd542b510d617535a678b4f49b7d3d4357
eduardo.voltolini <eduardoftl@hotmail.com>
2017-06-24 09:11:56 -0300

    final

--- MyEyesPlus/MyEyes_Interface/MainActivity.cs
+++ MyEyesPlus/MyEyes_Interface/MainActivity.cs
@@ -5,9 +5,12 @@ using Android.Content;
 using Android.Graphics;
 using Android.Provider;
 using Android.Runtime;
+using Android.Media;
 using System.Threading.Tasks;
 using System.Threading;
 using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 namespace MyEyes
@@ -15,22 +18,22 @@ namespace MyEyes
     [Activity(Label = "MyEyes", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        private int byteArray;
+        int cont;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            // Criação de botões
+            MediaPlayer _myPlayer = MediaPlayer.Create(this, DetectarAudio());
+
             Button BtnCamera = FindViewById<Button>(Resource.Id.Camera);
             Button BtnRepetir = FindViewById<Button>(Resource.Id.Repetir);
             Button BtnFechar = FindViewById<Button>(Resource.Id.Fechar);
             ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
 
-            // Definição dos metodos
             BtnCamera.Click += delegate { Camera(); };
-            BtnRepetir.Click += delegate { Repetir(); };
+            BtnRepetir.Click += delegate { Repetir(_myPlayer); };
             BtnFechar.Click += delegate { Fechar(); };
         }
 
@@ -41,34 +44,39 @@ namespace MyEyes
 
             ImageView CmpImageCamera = FindViewById<ImageView>(Resource.Id.ImageCamera);
             CmpImageCamera.SetImageBitmap(imageBitmap);
-        }
 
-        public static async Task<string> GetStreamStringFromImageSourceAsync(Xamarin.Forms.StreamImageSource imageSource, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            if (imageSource.Stream != null)
-            {
-                var stream = await imageSource.Stream(cancellationToken);
-
-                //stream?.Length > 0 can do this - Pubudu
-                // stream
-                if (stream == null || stream.Length <= 0) return null;
-
-                var bytes = new byte[stream.Length];
-                stream.Position = 0;
-                stream.Read(bytes, 0, (int)stream.Length);
-                return Convert.ToBase64String(bytes);
-            }
-            return null;
+            cont += 1;
+            MediaPlayer _myPlayer = MediaPlayer.Create(this, DetectarAudio());
+            _myPlayer.Start();
         }
 
         public void Camera()
         {
             Intent intent = new Intent(MediaStore.ActionImageCapture);
-            StartActivityForResult(intent, 0);
+            StartActivityForResult(intent, 0);            
         }
 
-        public void Repetir()
+        public void Repetir(MediaPlayer _myPlayer)
         {
+            _myPlayer.Start();
+        }
+
+        public int DetectarAudio()
+        {
+            switch (cont)
+            {
+                case 1:
+                    return Resource.Raw.EmNomedeOketra;
+                case 2:
+                    return Resource.Raw.TecelaDeCorrentes;
+                case 3:
+                    return Resource.Raw.PassagemParaAMaravilha;
+                case 4:
+                    return Resource.Raw.QuebraCabecaDoFlamiforjador;
+                default:
+                    cont = 0;
+                    return Resource.Raw.RufiaDaSerralheriaClandestina;
+            }
 
         }
 
@@ -83,7 +91,7 @@ namespace MyEyes
                 Finish();
             });
             alerta.SetNegativeButton("Cancelar", (senderAlert, args) =>
-            {});
+            { });
 
             Dialog dialog = alerta.Create();
             dialog.Show();
